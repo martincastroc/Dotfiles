b01a6013fdf22871a149991a0f093cbbb2b58c07
Martín Castro <castrom927@gmail.com>
2024-02-27 12:29:13 -0300

    Removed Ligatures

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,5 +9,10 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'Gruvbox Material (Gogh)'
 
+config.font = wezterm.font {
+    family = 'JetBrainsMono Nerd Font',
+    harfbuzz_features = {'calt=0','clig=0','liga=0'}
+}
+
 -- and finally, return the configuration to wezterm
 return config
