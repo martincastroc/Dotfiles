14b604d3cbfc9763db7c20aee4558174e62358c5
Martín Castro <castrom927@gmail.com>
2024-03-16 12:53:13 -0300

    Migrated away from lsp-zero

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,30 +1,75 @@
--- TODO: Migrate away from lsp-zero 
-local lsp = require('lsp-zero').preset({})
+return {
+    'neovim/nvim-lspconfig',
 
-lsp.ensure_installed({'tsserver','pyright','lua_ls'})
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        'L3MON4D3/LuaSnip',
+        {'j-hui/fidget.nvim', opts ={ } }
+    },
 
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-	end)
+    config = function()
+        vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+        vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+        vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = {buffer = event.buf}
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
+                -- these will be buffer-local keybindings
+                -- because they only work if you have an active language server
 
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set('n', 'rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                -- vim.keymap.set({'n', 'x'}, '<F2>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                vim.keymap.set('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            end
+        })
 
-lsp.setup()
+        local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-vim.diagnostic.config({
-  virtual_text = true
-})
+        local default_setup = function(server)
+            require('lspconfig')[server].setup({
+                capabilities = lsp_capabilities,
+            })
+        end
+
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            ensure_installed = {},
+            handlers = {
+                default_setup,
+            },
+        })
+
+        local cmp = require('cmp')
+
+        cmp.setup({
+            sources = {
+                {name = 'nvim_lsp'},
+            },
+            mapping = cmp.mapping.preset.insert({
+                -- Enter key confirms completion item
+                ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+                -- Ctrl + space triggers completion menu
+                ['<C-Space>'] = cmp.mapping.complete(),
+            }),
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+        })
+    end
+}
